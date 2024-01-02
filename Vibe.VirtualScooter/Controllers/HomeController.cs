8717eb5c6e991b99d49ba8d04a1c77a23fb6fb90
Vladislav Kasiyan <weazy@internet.ru>
2024-01-02 16:33:50 +0300

    Добавление методов передвижения, проверки доступности, вывода информации о виртуальном самокате

--- Vibe.VirtualScooter/Controllers/HomeController.cs
+++ Vibe.VirtualScooter/Controllers/HomeController.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using Vibe.Tools.Result;
+using Vibe.VirtualScooter.Modules;
+
+public class HomeController : Controller
+{
+    public HomeController() { }
+
+    [HttpGet("PrintScooterInfo")]
+    public void PrintScooterInfo()
+    {
+        VirtualScooter.Instance.PrintVirtualScooterInfo();
+    }
+
+    [HttpGet("CheckScooterAvailability")]
+    public Result CheckScooterAvailability()
+    {
+        return VirtualScooter.Instance.CheckScooterAvailability();
+    }
+
+    [HttpGet("ScooterMove")]
+    public void ScooterMove(Double x, Double y)
+    {
+        VirtualScooter.Instance.MoveTo(x, y);
+    }
+}
