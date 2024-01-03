ae2e045b0645379376dde4a100858ea599099cf4
Vladislav Kasiyan <weazy@internet.ru>
2024-01-03 15:51:50 +0300

    Удаление дефолтного кода в Backoffice

--- Vibe.Backoffice/Vibe.BackOffice.Server/Controllers/WeatherForecastController.cs
+++ Vibe.Backoffice/Vibe.BackOffice.Server/Controllers/WeatherForecastController.cs
@@ -1,33 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
+using Vibe.Domain.Scooter;
 
 namespace Vibe.BackOffice.Server.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        public WeatherForecastController()
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
         }
     }
 }
