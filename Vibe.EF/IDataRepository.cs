23dbc3cd75d5991331208eb52b336f85d82b3f71
Vladislav Kasiyan <weazy@internet.ru>
2024-01-03 18:49:17 +0300

    Добавление абстрактного репозитория

---
+++ Vibe.EF/IDataRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Vibe.EF
+{
+    public interface IDataRepository <TEntity>
+    {
+        void Save(TEntity entity);
+        IEnumerable<TEntity> List();
+        TEntity? Get(Guid id);
+        void Remove(Guid id);
+        void Update(TEntity entity);
+    }
+}
