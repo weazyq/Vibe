86e4bcf5c8750dfe3fdd62907fb05211c973a53c
Vladislav Kasiyan <weazy@internet.ru>
2024-01-07 14:16:53 +0300

    Изменены Auditable, IAuditable

--- Vibe.EF/Entities/Base/Auditable.cs
+++ Vibe.EF/Entities/Base/Auditable.cs
@@ -4,8 +4,8 @@ namespace Vibe.EF.Entities.Base
     public abstract class Auditable : IAuditable
     {
         public DateTime CreatedAt { get; set; }
-        public Guid CreatedBy { get; set; }
-        public DateTime ModifiedAt { get; set; }
-        public Guid UpatedBy { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public Guid? UpdatedBy { get; set; }
     }
 }
--- Vibe.EF/Entities/Base/IAuditable.cs
+++ Vibe.EF/Entities/Base/IAuditable.cs
@@ -3,8 +3,8 @@
     public interface IAuditable
     {
         DateTime CreatedAt { get; set; }
-        Guid CreatedBy { get; set; }
-        DateTime ModifiedAt { get; set; }
-        Guid UpatedBy { get; set; }
+        Guid? CreatedBy { get; set; }
+        DateTime? ModifiedAt { get; set; }
+        Guid? UpdatedBy { get; set; }
     }
 }
