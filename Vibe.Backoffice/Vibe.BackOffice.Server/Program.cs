bc674ab6df4ef4750137789d00b80e014e3d0f34
Vladislav Kasiyan <weazy@internet.ru>
2024-02-17 14:09:36 +0300

    Добавление токенов

--- Vibe.Backoffice/Vibe.BackOffice.Server/Program.cs
+++ Vibe.Backoffice/Vibe.BackOffice.Server/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Vibe.BackOffice.Server.Tools;
 using Vibe.EF;
 using Vibe.EF.Entities;
 using Vibe.EF.Interface;
@@ -33,6 +37,35 @@ builder.Services.AddDbContext<DataContext>(options => options
     .UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
     .UseLowerCaseNamingConvention()
 );
+
+builder.Services.Configure<JWTSettings>(builder.Configuration.GetSection(nameof(JWTSettings)));
+
+String? secretKey = builder.Configuration.GetSection($"{nameof(JWTSettings)}:{nameof(JWTSettings.SecretKey)}").Value;
+var jwtSettings = new
+{
+    Issuer = builder.Configuration.GetSection($"{nameof(JWTSettings)}:{nameof(JWTSettings.Issuer)}").Value,
+    Audience = builder.Configuration.GetSection($"{nameof(JWTSettings)}:{nameof(JWTSettings.Audience)}").Value,
+    SigningKey = JWTTools.FormSingingKey(secretKey!)
+};
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = jwtSettings.Issuer,
+        ValidateAudience = true,
+        ValidAudience = jwtSettings.Audience,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = jwtSettings.SigningKey,
+        ValidateLifetime = true
+    };
+});
 
 var app = builder.Build();
 
