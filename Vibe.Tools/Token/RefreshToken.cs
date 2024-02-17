272bd665971bfa5e1d46e49a84a02616b278aed9
Vladislav Kasiyan <weazy@internet.ru>
2024-02-17 16:23:37 +0300

    Добавление RefreshToken'a

---
+++ Vibe.Tools/Token/RefreshToken.cs
@@ -0,0 +1,9 @@
+﻿namespace Vibe.Tools.Token
+{
+    public class RefreshToken
+    {
+        public String Token { get; set; } = String.Empty;
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Expires { get; set; }
+    }
+}
