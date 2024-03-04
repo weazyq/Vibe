965129de2a418bd00660c2264c88ec0cf05720b5
Weazy <weazy@internet.ru>
2024-03-04 21:26:57 +0300

    Исправление отсутствия Vibe.Configurator в репозитории

---
+++ Vibe.Configurator/Configuration/JWTSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Vibe.Configurator.Configuration;
+
+public static class JWTSettings
+{
+    public static String SecretKey { get; set; } = String.Empty;
+    public static String Issuer { get; set; } = String.Empty;
+    public static String Audience { get; set; } = String.Empty;
+}
---
+++ Vibe.Configurator/Configuration/VirtualScooterSettings.cs
@@ -0,0 +1,5 @@
+﻿namespace Vibe.Configurator.Configuration;
+public static class VirtualScooterSettings
+{
+    public static String Host { get; set; } = String.Empty;
+}
