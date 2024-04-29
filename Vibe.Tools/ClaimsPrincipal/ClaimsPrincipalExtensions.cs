419c692d6efb7b751adb898b1fa3f69aefe1e8f0
Weazy <weazy@internet.ru>
2024-04-29 13:18:16 +0300

    Добавлена возможность получения UserId из Claims

---
+++ Vibe.Tools/ClaimsPrincipal/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace Vibe.Tools.ControllerExtenstions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid GetUserId(this ClaimsPrincipal User)
+        {
+            return new Guid(User.Claims.First(i => i.Type == ClaimTypes.NameIdentifier).Value);
+        }
+    }
+}
